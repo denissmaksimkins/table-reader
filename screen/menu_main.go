f1bb2feeb49a57db00264afec4d546d661f52a3b
Ričards Šašalevičs <rsasalevics@edu.riga.lv>
2022-05-09 10:30:45 +0300

    base code

---
+++ screen/menu_main.go
@@ -0,0 +1,35 @@
+package screen
+
+import (
+	"eklase/state"
+
+	"gioui.org/layout"
+	"gioui.org/widget"
+	"gioui.org/widget/material"
+)
+
+// MainMenu defines a main menu screen layout.
+func MainMenu(th *material.Theme, state *state.State) Screen {
+	var (
+		add  widget.Clickable
+		list widget.Clickable
+		quit widget.Clickable
+	)
+	return func(gtx layout.Context) (Screen, layout.Dimensions) {
+		d := layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+			layout.Rigid(rowInset(material.Button(th, &add, "Add student").Layout)),
+			layout.Rigid(rowInset(material.Button(th, &list, "List students").Layout)),
+			layout.Rigid(rowInset(material.Button(th, &quit, "Quit").Layout)),
+		)
+		if add.Clicked() {
+			return AddStudent(th, state), d
+		}
+		if list.Clicked() {
+			return ListStudent(th, state), d
+		}
+		if quit.Clicked() {
+			state.Quit()
+		}
+		return nil, d
+	}
+}
