16da7eb81ec8df57a68d52f48d349a3ec4a9bc60
Denis <denismax328@gmail.com>
2022-05-12 00:02:18 +0300

    delete button

--- screen/menu_main.go
+++ screen/menu_main.go
@@ -11,15 +11,17 @@ import (
 // MainMenu defines a main menu screen layout.
 func MainMenu(th *material.Theme, state *state.State) Screen {
 	var (
-		add  widget.Clickable
-		list widget.Clickable
-		quit widget.Clickable
+		add    widget.Clickable
+		list   widget.Clickable
+		quit   widget.Clickable
+		delete widget.Clickable
 	)
 	return func(gtx layout.Context) (Screen, layout.Dimensions) {
 		d := layout.Flex{Axis: layout.Vertical}.Layout(gtx,
 			layout.Rigid(rowInset(material.Button(th, &add, "Add student").Layout)),
 			layout.Rigid(rowInset(material.Button(th, &list, "List students").Layout)),
 			layout.Rigid(rowInset(material.Button(th, &quit, "Quit").Layout)),
+			layout.Rigid(rowInset(material.Button(th, &delete, "Delete").Layout)),
 		)
 		if add.Clicked() {
 			return AddStudent(th, state), d
