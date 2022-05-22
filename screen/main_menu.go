2f7d7d653ad2f394e237605c9adb3c6c3f56c5cf
Andrej Ceremnih <andrejceremnih@gmail.com>
2022-05-22 13:34:41 +0300

    tried to change widget's colour to more gray, but couldn't

--- screen/main_menu.go
+++ screen/main_menu.go
@@ -18,6 +18,7 @@ func MainMenu(th *material.Theme, state *state.State) Screen {
 		quit widget.Clickable
 	)
 	return func(gtx layout.Context) (Screen, layout.Dimensions) {
+		// widgetColour(gtx)
 		matAddBut := material.Button(th, &add, "Add student")
 		matAddBut.Font = text.Font{Variant: "Mono", Weight: text.Bold, Style: text.Italic}
 		matAddBut.Background = color.NRGBA{A: 0xff, R: 0x1e, G: 0x4d, B: 0x24}
